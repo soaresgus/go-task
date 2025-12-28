bca3b361d78a4bd2bcb6ccc1cd526f4f52bf983b
soaresgus <gustavosoaresbt@hotmail.com>
2025-12-28 13:08:26 -0300

    feat: created welcome section component style

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { WelcomeSectionComponent } from '../welcome-section/welcome-section.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [WelcomeSectionComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css',
 })
