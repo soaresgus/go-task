72d6f2f8655a0bbdb28795335a2afa170733fe10
soaresgus <gustavosoaresbt@hotmail.com>
2026-01-02 21:32:15 -0300

    feat: created form control for comments section and added dynamism for form control

--- src/app/components/task-comments-modal/task-comments-modal.component.ts
+++ src/app/components/task-comments-modal/task-comments-modal.component.ts
@@ -2,17 +2,23 @@ import { DIALOG_DATA } from '@angular/cdk/dialog';
 import { Component, inject } from '@angular/core';
 import { ITask } from '../../interfaces/task.interface';
 import { formatElapsed } from '../../utils/format-elapsed';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-comments-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-comments-modal.component.html',
   styleUrl: './task-comments-modal.component.css',
 })
 export class TaskCommentsModalComponent {
+  commentControl = new FormControl('', [Validators.required]);
   readonly _task = inject<ITask>(DIALOG_DATA);
 
   formatElapsed = (date: Date | string): string => {
     return formatElapsed(date);
   };
+
+  onAddComment() {
+    console.log('comment', this.commentControl.value);
+  }
 }
